5a2c5d4286c9626710a418e13f7e7923939e38d0
A.Stoychev <109471350+AStoychev@users.noreply.github.com>
2023-01-24 16:02:35 +0200

    add routes and cotrollers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 
+const routes = require('./routes')
 const config = require('./config');
 const setupViewEngine = require('./config/viewEngine');
 
@@ -7,11 +8,7 @@ const app = express();
 setupViewEngine(app);
 
 app.use(express.static('src/public'));
-
-app.get('/', (req, res) => {
-    res.render('home');
-    // res.send('Home page');
-});
+app.use(routes);
 
 app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`)); 
 
