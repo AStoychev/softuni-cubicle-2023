97ef757a3d6839e22d7577bf09054e571c0b57f9
A.Stoychev <109471350+AStoychev@users.noreply.github.com>
2023-01-25 08:53:48 +0200

    last update json

---
+++ src/models/Cube.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const db = require('../db.json');
+const path = require('path');
+
+class Cube {
+    constructor(name, description, imageURL, difficultyLevel) {
+        this.name = name;
+        this.description = description;
+        this.imageURL = imageURL;
+        this.difficultyLevel = difficultyLevel;
+    }
+
+    static save(cube) {
+        db.cubes.push(cube);
+        const jsonData = JSON.stringify(db, 2);
+        fs.writeFileSync(path.resolve(__dirname, '../db.json'), jsonData)
+    }
+}
+
+module.exports = Cube;
--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,9 @@ const cubeController = require('./controllers/cubeController');
 const homeController = require('./controllers/homeController');
 
 router.get('/', homeController.getHomePage);
-
 router.get('/about', homeController.getAboutPage);
 
 router.get('/create', cubeController.getCreateCube);
+router.post('/create', cubeController.postCreateCube);
 
 module.exports = router;
